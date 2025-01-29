0305e6b4e8bf0f7cec591b00df294d88b9e56e66
Felipe <Felipe@teste.com>
2025-01-29 11:23:57 -0300

    atualizando o projeto

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ function veiculo(moto) {
     this.loja = function() {
         console.log(this.modelo + " motors");
     }
+    this.carroECia = function() {
+        console.log(this.ferrari + " E luxos");
+    }
 
 }
 
@@ -28,8 +31,53 @@ function marca(moto, modelo, ano) {
     veiculo.call(this, moto);
 }
 
+function duasRodas(honda, yamaha, ducati) {
+    this.honda = honda;
+    this.yamaha = yamaha;
+    this.ducati = ducati
+
+    this.mthonda = function() {
+        console.log(this.honda);
+    }
+
+    this.mtyamaha = function() {
+        console.log(this.yamaha);
+    }
+
+    this.mtducati = function() {
+        console.log(this.ducati);
+    }
+
+    veiculo.call(this.moto);
+}
+
+function carroEMoto(ferrari, porsche, lamborghini) {
+    this.ferrari = ferrari;
+    this.porsche = porsche;
+    this.lamborghini = lamborghini;
+
+    this.carferrari = function() {
+        console.log(this.ferrari);
+    }
+    this.carporsche = function() {
+        console.log(this.porsche);
+    }
+    this.carlamborghini = function() {
+        console.log(this.lamborghini);
+    }
+
+    veiculo.call(this.moto);
+}
+
+
+
+
+
 const marca1 = new marca("CBR", "Honda", 2020);
 const marca2 = new marca ("tiger-900", "triumph", 2023);
+const marca3  = new duasRodas("CBR", "R1", "ducati");
+const carro1 = new carroEMoto("Ferrari 458", "GT3 RS", "Urus");
+
 marca1.empresa();
 marca1.dizmoto();
 marca1.dizmodelo();
@@ -40,4 +88,11 @@ marca2.dizmoto();
 marca2.dizmodelo();
 marca2.dizano();
 
+marca3.mthonda();
+marca3.mtyamaha();
+marca3.mtducati();
+
+carro1.carferrari();
+carro1.carporsche();
+carro1.carlamborghini();
 
